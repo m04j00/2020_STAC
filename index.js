08958f5ec70cabda68d8a70e0d1619298bbafa74
이민지 <69101860+m04j00@users.noreply.github.com>
2020-09-19 04:26:37 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+process.send = process.send || function () {};
 
 const indexRouter = require('./router/index');
 const usersRouter = require('./router/user');
