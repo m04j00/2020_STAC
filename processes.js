05f073c98f6a3e3a42b115758c60080a4b4c9343
이민지 <69101860+m04j00@users.noreply.github.com>
2020-09-19 02:16:44 +0900

    cluster

--- processes.js
+++ processes.js
@@ -1,8 +1,8 @@
 module.exports = {
     apps: [{
-    name: 'index',
-    script: './index.js',
+    name: "index",
+    script: "./index.js",
     instances: 0,
-    exec_mode: ‘cluster’
+    exec_mode: "cluster"
     }]
   }
