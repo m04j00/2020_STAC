2711ede58eaa9dc115d9cdc51ff71b68d5ec7c13
이민지 <69101860+m04j00@users.noreply.github.com>
2020-09-19 04:24:09 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-process.send = process.send || function () {};
-
-let isDisableKeepAlive = false;
-app.use(function(request, response, next) {
-  if (isDisableKeepAlive) {
-    response.set('Connection', 'close');
-  }
-  next();
-});
 
 const indexRouter = require('./router/index');
 const usersRouter = require('./router/user');
@@ -23,10 +14,3 @@ app.listen(port, function() {
   process.send('ready')
   console.log(`application is listening on port ${port}...`)
 });
-
-process.on('SIGINT', async () => {
-  isDisableKeepAlive = true;
-  await app.close();
-  console.log('server closed');
-  process.exit(0);
-  });
