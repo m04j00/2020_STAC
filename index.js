baf0210f8a66d4edde8d2e53e93d93f05ba49422
이민지 <69101860+m04j00@users.noreply.github.com>
2020-09-19 17:04:16 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+'use strict';
+
 const express = require('express');
 const app = express();
 const port = 3000;
+const HOST = '0.0.0.0';
 process.send = process.send || function () {};
+
+let isDisableKeepAlive = false;
+app.use(function(request, response, next) {
+  if (isDisableKeepAlive) {
+    response.set('Connection', 'close');
+  }
+  next();
+});
 
 const indexRouter = require('./router/index');
 const usersRouter = require('./router/user');
@@ -12,6 +23,15 @@ app.use('/user', usersRouter);
 app.use('/recode', recodeRouter);
 
 app.listen(port, function() {
-  process.send('ready')
-  console.log(`application is listening on port ${port}...`)
+  process.send('ready');
+  console.log(`Running on http://${HOST}:${PORT}`);
+});
+
+process.on('SIGINT', function() { 
+  console.log('> received SIGNIT signal');
+  isAppGoingToBeClosed = true;
+  listeningServer.close(function(err) {
+    console.log('server closed');
+    process.exit(err ? 1 : 0);
+   });
 });
